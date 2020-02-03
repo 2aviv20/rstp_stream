ef9e2d223eeaa609aeb6256de164df626fec8ca6
aviv <aviv@cycurity.com>
2020-02-04 00:01:19 +0200

    add docker

--- nodeServer/db/db.js
+++ nodeServer/db/db.js
@@ -1,10 +1,24 @@
-const mongoose = require('mongoose')
-console.log(process.env.MONGODB_URL);
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-})
+// const mongoose = require('mongoose')
+// console.log(process.env.MONGODB_URL);
+// mongoose.connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+// })
 
-mongoose.connection.on('connected', function(){
-    console.log("mongo connected")
-});
+// mongoose.connection.on('connected', function(){
+//     console.log("mongo connected")
+// });
+
+
+var mongoose = require('mongoose')
+var mongoUrl = process.env.MONGODB_URL
+
+var connectWithRetry = function() {
+  return mongoose.connect(mongoUrl, function(err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      setTimeout(connectWithRetry, 10000);
+    }
+  });
+};
+connectWithRetry();
